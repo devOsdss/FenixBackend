69a591c330660ef41643a85a7115a1013f714647
devOsdss <marichynpetro@gmail.com>
2025-09-25 17:15:21 +0300

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:3001'], // Frontend URLs
+  origin: ['www.international-payments.cc', ], // Frontend URLs
   credentials: true
 }));
 app.use(express.json());
